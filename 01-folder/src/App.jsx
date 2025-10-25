1a919fc9d329243981af9a896a9e570b260645aa
Sujith-BN <sujithbn01@gmail.com>
2025-10-25 17:55:23 +0530

    01-folder-done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div className="sujith">
+        <h1>im the hero</h1>
+    </div>
+    <div className="spoorthi">
+        <h2>his wife</h2>
+    </div>
+    </>
+   
+  )
+}
+
+export default App
